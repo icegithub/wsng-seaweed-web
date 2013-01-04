2185ef04f4ae872db6cbc35fb88f8de333bb8e55
icecream <creamidea@gmail.com>
2013-01-04 21:23:51 +0800

    Continue

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.configure(function(){
     // });
 })
 //同时支持html的设置
-app.register('html', require('ejs')); 
+// app.register('html', require('ejs')); 
 
 // 定义开发环境
 app.configure('development', function(){
