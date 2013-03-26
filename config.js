402b36dfcc00739c470cd1e1ba8e7efd575f1b8d
icecream <creamidea@gmail.com>
2013-03-26 13:45:02 +0800

    Add description

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 
 exports.siteName = 'icecream';
 exports.siteDesc = 'I am here';
+exports.siteVersion = 'v2.0';
 
 // exports.mongodb = 'mongodb://icecream:njjizyj0826@ds047197.mongolab.com:47197/icecream-nodester'
 // exports.mongodb = 'mongodb://127.0.0.1:27017/icecream-nodester'
